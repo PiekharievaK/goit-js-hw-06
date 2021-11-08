4da88c217885b77cd2aa34712b4ea338d774bb1d
PiekharievaK <pechareva@gmail.com>
2021-11-08 20:03:10 +0200

    Решение задачи 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientList = document.querySelector(`#ingredients`);
+
+const result = [];
+ingredients.forEach(ingredient => {
+ const element = document.createElement(`li`);
+ element.textContent =`${ingredient}`; 
+ element.className = `item`;
+ result.push(element)
+} );
+ 
+  ingredientList.append(...result);
+
+  console.log(ingredientList);
+
+
